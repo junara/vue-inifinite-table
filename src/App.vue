fcfc89eefe07fe3f77a5a800c22181d6874bba4d
junara <jun5araki@gmail.com>
2023-12-25 08:20:25 +0900

    Implement infinite scroll feature in App.vue
    This update replaces static content on App.vue with a new infinite scroll feature. Unused assets and components have also been removed. The infinite scroll uses Vue's ref lifecycle hook and useInfiniteScroll function from Vueuse library for dynamic list rendering.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,69 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div ref="itemListRef" style="overflow-y: scroll; height: 200px; border: solid">
+    <div v-for="item in items">
+      {{ item.id }}
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
+  <div>{{ `items.length = ${items.length}` }}</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue';
+import { useInfiniteScroll } from '@vueuse/core';
+interface Item {
+  id: number;
 }
+const initialId = ref(0);
+const initialItems = Array.from(Array(20).keys()).map((i) => ({ id: initialId.value + i }));
+const items = ref<Item[]>(initialItems);
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const itemListRef = ref<HTMLElement | null>(null);
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+onMounted(() => {
+  itemListRef.value?.scrollTo(0, itemListRef.value.scrollTop + distanceBottom + 1);
+});
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+const distanceTop = 10;
+const { isLoading: isLoadingTop } = useInfiniteScroll(
+  itemListRef,
+  () => {
+    // 連続して発火しないようにする
+    itemListRef.value?.scrollTo(0, itemListRef.value.scrollTop + distanceTop + 1);
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+    items.value.unshift({ id: firstItem.value.id - 1 });
+    items.value.unshift({ id: firstItem.value.id - 1 });
+    items.value.unshift({ id: firstItem.value.id - 1 });
+  },
+  { distance: distanceTop, direction: 'top' }
+);
+
+const distanceBottom = 10;
+const { isLoading: isLoadingBottom } = useInfiniteScroll(
+  itemListRef,
+  () => {
+    // 連続して発火しないようにする
+    itemListRef.value?.scrollTo(0, itemListRef.value.scrollTop - distanceBottom - 1);
+
+    items.value.push({ id: lastItem.value.id + 1 });
+    items.value.push({ id: lastItem.value.id + 1 });
+    items.value.push({ id: lastItem.value.id + 1 });
+  },
+  { distance: distanceBottom, direction: 'bottom' }
+);
+
+const firstItem = computed(() => {
+  return items.value.find((item) => item.id === minId.value)!;
+});
+
+const lastItem = computed(() => {
+  return items.value.find((item) => item.id === maxId.value)!;
+});
+
+const minId = computed(() => {
+  return Math.min(...items.value.map((item) => item.id));
+});
+
+const maxId = computed(() => {
+  return Math.max(...items.value.map((item) => item.id));
+});
+</script>
